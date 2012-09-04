dc6fd4dcbca2fe153007625f50bbe09038b44180
Vasilii Pavlov <shagrathxd@gmail.com>
2012-09-04 18:51:10 +0400

    deleted unused data

--- Classes/Objects/Torpedo.cs
+++ Classes/Objects/Torpedo.cs
@@ -11,15 +11,7 @@ namespace TorpedoModel.Classes
        TorpedoControlAlgorithm alg;
        Target targ;
        
-       /// <summary>
-       /// //////////
-       /// </summary>
-           public double speed = 0;  //скорость торпеды
-           public string emitterMode = "C1";   //режим излучения
-           public Angle rudderCourse; //углы поворота рулей
-       /// <summary>
-       /// /////
-       /// </summary>
+
            public Coordinates torpPosition; //координаты торпеды
 
            public Coordinates coord = new Coordinates();  //инициализация координат
@@ -27,54 +19,21 @@ namespace TorpedoModel.Classes
            {
                torpPosition = coord_;
            }
-      /// <summary>
-      /// //////////////
-      /// </summary>
-           public Angle rudder = new Angle(0, 0); //инициализация руля
-           public void InitRudder(Angle rudder_)
-           {
-               rudderCourse = rudder_;
-           }
-      /// <summary>
-      /// /////////////
-      /// </summary>
+
 
            public Torpedo()     //инициализация всей торпеды
            {
                InitCoord(coord);
                TorpedoControlAlgorithm alg_ = new TorpedoControlAlgorithm();
                alg = alg_;
-               /// <summary>
-               /// /////////////
-               /// </summary>  
-               InitRudder(rudder);
-               /// <summary>
-               /// /////////////
-               /// </summary>
-           }
+            }
 
            public void SetTorpedoPosition(Coordinates torpedoPosition) //запись координат торпеды
            {                   
                torpPosition = torpedoPosition;
            }
 
-           /// <summary>
-           /// /////////////
-           /// </summary>
-           public void SetControlData(Torpedo controlData)  //установка параметров управления
-           {
-               speed = controlData.speed;
-               emitterMode = controlData.emitterMode;
-               rudderCourse = controlData.rudderCourse;
-           }
-          
-           public void GetSpeed(out double speed_)   //получить значение скорости
-           {
-               speed_ = speed;
-           }
-        /// <summary>
-        /// /////////////
-        /// </summary>
+
 
            public void GetTargetData(Target targ_)      //получить информацию о цели
            {
