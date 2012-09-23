873aa1e52151fdad3d9296d78d9171ed82474848
Vasilii Pavlov <shagrathxd@gmail.com>
2012-09-24 01:58:07 +0400

    Добавлена реализация интерфейса IObjectControl, удален лишний код

--- Classes/TorpedoControlAlgorithm.cs
+++ Classes/TorpedoControlAlgorithm.cs
@@ -9,33 +9,26 @@ namespace TorpedoModel
 {
     public class TorpedoControlAlgorithm
     {
+        IObjectControl controlledObject;
         public const int d2_ = 700; //дистанция, на которой режим излучения меняется с "С1" на "С2"
         public const int d3_ = 500;  //дистанция, на которой длительность акустического цикла уменьшается в 4 раза, м
         public const int d4_ = 200;      //дистанция, на которой длительность зондирующего сигнала уменьшается в 2 раза, м
         private bool searchIsFirst_ = true;     //указатель того, что поиск первичный
         
-        public Target targ = new Target();
+        public Target targ;
 
-        public TorpedoControlAlgorithm()
-            {
-            }
-
-
-        public TorpedoControlAlgorithm(Target target)
+        public TorpedoControlAlgorithm(IObjectControl o)
         {
-            
-            targ = target;
+            controlledObject = o; 
         }
-
 
         public void Process()  //алгоритм
         {
-            
-            if (targ.isReceived = true)
+            if (targ.isReceived == true)
                 Convergence();
             else
             {
-                if (searchIsFirst_ = true)
+                if (searchIsFirst_ == true)
                     FirstSearch();
                 else
                     RepeatedSearch();
@@ -47,34 +40,23 @@ namespace TorpedoModel
         {
             switch ((int)(targ.distance))
             {
-                case d4_:
-                    torp.emitterMode = "C4";
+                case (d4_):
+                    controlledObject.SetEmitterMode("C4");
                     break;
                 case (d3_):
-                    torp.emitterMode = "C3";
+                    controlledObject.SetEmitterMode("C3");
                     break;
                 case (d2_):
-                    torp.emitterMode = "C2";
+                    controlledObject.SetEmitterMode("C2");
                     break;
                 default:
                     break;
             }
+        }
 
-            
-  ///          if (targ.distance < d2_)
-  ///          {
-  ///              if (targ.distance < d3_)
-  ///              {
-  ///                  if (targ.distance < d4_)
-  ///                      torp.emitterMode = "C4";
-  ///                  else
-  ///                      torp.emitterMode = "C3";
-  ///              }
-  ///              else
-  ///                  torp.emitterMode = "C2";
-  ///          }
-            ///          else
-            ///              torp.emitterMode = "C1";
+        public void AddTarget(Target t)
+        {
+            targ = t;
         }
 
         public void FirstSearch()   //первичный поиск
@@ -89,8 +71,5 @@ namespace TorpedoModel
         public void ContactVerification()   //подтверждение контакта
         {
         }
-
-       
-
     }
 }
