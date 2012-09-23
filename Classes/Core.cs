88d81640e04afab48999878d084a2e43d2b9d5be
Vasilii Pavlov <shagrathxd@gmail.com>
2012-09-24 01:59:57 +0400

    Deleted unused code

--- Classes/Core.cs
+++ Classes/Core.cs
@@ -14,7 +14,6 @@ namespace TorpedoModel
         private bool stop_ = false;  //флаг остановки 
         MovingProcessor move;
         ObjectControl obj; 
-        TorpedoControlAlgorithm alg;
         Timer timer = new Timer();
         
         public void Cycle() //цикл последовательно вызывает основные компоненты системы
@@ -23,16 +22,9 @@ namespace TorpedoModel
             move = move_;
             move.InitMovingProcessor();
             Console.WriteLine("init move proc ok");
-            TorpedoControlAlgorithm alg_ = new TorpedoControlAlgorithm(move.torp, move.targ);
-            alg = alg_;
-            ObjectControl obj_ = new ObjectControl();
-            obj = obj_;
             while (!stop_)  
             {
-                alg.Process();
-                move.torp.SetControlData(alg.torp);
                 move.MoveProcess();
-                alg.targ.SetTargetData(move.targ);
                 timer.Increment(step_);             
             }
         }  
