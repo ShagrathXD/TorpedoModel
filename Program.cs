621a849113954413ea9808cd53f88faba03a68c8
Vasilii Pavlov <shagrathxd@gmail.com>
2012-09-25 03:43:30 +0400

    syntax error

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using TorpedoModel.Classes;
+using TorpedoModel.Classes; 
 using TorpedoModel.Interfaces;
 ///работа системы начинается с запуска инициализации в ядре. этот основной класс приложения служит для передачи управления в ядро
 namespace TorpedoModel
