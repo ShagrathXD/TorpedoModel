ee47ef887bd381a5a0a40f2f90f8b3d860cc209c
Vasilii Pavlov <shagrathxd@gmail.com>
2012-10-08 08:01:55 +0400

    исправлена ошибка

--- Interfaces/ITorpedoControlAlgorithm.cs
+++ Interfaces/ITorpedoControlAlgorithm.cs
@@ -9,7 +9,7 @@ namespace TorpedoModel.Interfaces
 {
     public interface ITorpedoControlAlgorithm      //интерфейс, по которому в алгоритм пересылаются данные
     {
-        void SetTargetInformation(Angle peleng, float distnce, Angle targetCourse, float targetSpeed);
+        void SetTargetInformation(Angle peleng, float distance, Angle targetCourse, float targetSpeed);
         void Process(); //Запустить работу алгоритма
         void SetTargetSignalReceived(bool isReceived);//сигнализация о том, получен сигнал от цели или нет
         void SetSignalAmplitude(float signalAmp); //уровень амплитуды сигнала
