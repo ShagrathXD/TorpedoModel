94f0e9e2181a13fa20af133e89945db656fbaa92
Vasilii Pavlov <shagrathxd@gmail.com>
2012-10-08 06:58:19 +0400

    Обновлен интерфейс управления

--- Interfaces/IObjectControl.cs
+++ Interfaces/IObjectControl.cs
@@ -7,9 +7,13 @@ namespace TorpedoModel.Interfaces
 {
     public interface IObjectControl     //интерфейс, по которому алгоритм управляет торпедой
     {
-        void SetRudderCourse(Classes.Angle angle); 
-        void SetSpeed(float speed);
-        void SetEmitterMode(string mode); //Функция задающая тип сигнала излучателя, строка - имя режима. 
-        void SetCockedMode(bool mode);  //установка боевого взвода
+        void SetHorizontalRotationVelocity(float newSpeed); //скорость горизонтальной циркуляции
+        void SetVerticalRotationVelocity(float newSpeed);   //скорость вертикальной циркуляции
+        void SetEnginePower(float speed); //установка скорости
+        void SetCockedMode(bool on); //установка боевого взвода
+        void SetSignalLength(float length); //установка длительности излучения
+        void SetProbeLength(float length);  //установка периода зондирования (времени ожидания сигнала)
+        void EmmitSignalAndStartEchoWaiting(); //команда на излучение и ожидание сигнала
+        void StopEchoWaiting();     //команда на прекращение ожидания сигнала
     }
 }
