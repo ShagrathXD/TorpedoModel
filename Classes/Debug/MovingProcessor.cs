15130ceba8829173219663a58e6a07b95d30e885
Vasilii Pavlov <shagrathxd@gmail.com>
2012-10-09 02:35:05 +0400

    Сгруппировал код для удобства восприятия

--- Classes/Debug/MovingProcessor.cs
+++ Classes/Debug/MovingProcessor.cs
@@ -13,6 +13,19 @@ namespace TorpedoModel
         Torpedo torp;   //торпеда   
         Target targ;    //цель
 
+        public MovingProcessor()
+        {
+            CreateBasicObjects();
+        }
+
+        #region Creating_Basic_Objects
+
+        public void CreateBasicObjects() //создание начальных объектов
+        {
+            AddTorpedo(new Torpedo(new Coordinates(), new ObjectControl()));  //создание торпеды с нулевыми координатами и нулевыми параметрами управления
+            AddTarget(new Target(new Angle(), 99999, new Angle(), 0)); //создание цели на дистанции 99999 с нулевыми углами курса/пеленга и нулевой скоростью 
+        }
+
         void AddTorpedo(Torpedo newTorp)   //добавить новую торпеду
         {
             torp = newTorp;
@@ -23,17 +36,18 @@ namespace TorpedoModel
             targ = newTarget;
         }
 
-        public void CreateObjects() //создание начальных объектов
-        {   
-            AddTorpedo( new Torpedo(new Coordinates(), new ObjectControl()));  //создание торпеды с нулевыми координатами и нулевыми параметрами управления
-            AddTarget( new Target(new Angle(), 99999, new Angle(), 0)); //создание цели на дистанции 99999 с нулевыми углами курса/пеленга и нулевой скоростью 
-        }
+        #endregion       
 
         public void MoveProcess()  //main     
         {
-            torp.Algorithm(targ.peleng, targ.distance, targ.course, targ.speed);       //запуск алгоритма
+            torp.Algorithm(targ.peleng, targ.distance, targ.course, targ.speed);    //запуск алгоритма. Осуществляется передача данных 
+            //от цели targ в алгоритм Algorithm через торпеду torp
+
+            #region Moving
             targ.distance = targ.distance - torp.GetSpeed();     //изменение значения дистанции
             torp.coord.x = torp.coord.x + torp.GetSpeed();       //изменение координат
+            #endregion 
+
         }
     }
 }
