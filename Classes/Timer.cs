019392f3defe3dbaa212bcfff908db56002f66f3
Vasilii Pavlov <shagrathxd@gmail.com>
2012-10-09 03:00:28 +0400

    Добавил комментарии, поправил оформление

--- Classes/Timer.cs
+++ Classes/Timer.cs
@@ -4,37 +4,34 @@ using System.Text;
 
 namespace TorpedoModel.Classes
 {
- public class Timer     //системный таймер
-        {          
-            private static double time_;
+    public class Timer     //класс реализует все, что связано со временем 
+    {          
+        private static double time_;
 
-            float countIInterval;
-            bool timeIsOut; 
+        float countInterval;   //интервал работы обратного таймера 
+        bool timeIsOut;        //сообщает о том, что время истекло
 
-            public Timer()
-            {
-                Reset();
-            }
-
-            public static void Reset()	  //обнуление
-            {
-                time_ = 0;
-            }
-            public void Increment(double step)  //время + 1
-            {
-                time_ += step;
-            }
-            public static void GetTime(out double time)  //значение времени
-            {
-                time = time_;
-            }
-            public void Count(float countInterval_)
-            {
-                // таймер, начинающий свою работу при вызове и 
-                // заканчивающий через время timeInterval_
-            }
-
+        public Timer()      //конструктор 
+        {
+            Reset();
         }
-        
-        
+
+        public static void Reset()	  //обнуление
+        {
+            time_ = 0;
+        }
+        public void Increment(double step)  //время + 1
+        {
+            time_ += step;
+        }
+        public static void GetTime(out double time)  //значение времени
+        {
+            time = time_;
+        }
+        public void Count(float countInterval_)
+        {
+            // таймер, начинающий свою работу при вызове и 
+            // заканчивающий через время timeInterval_
+        }
+    }    
 }
